43f6488beb195291078453861a2d0b87b3b5f7bf
SashaGolovach <gsancho0001@gmail.com>
2020-11-28 00:25:36 +0200

    Status controller added

---
+++ Connected-Backend/Connected-API/Controllers/StatusController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Connected.Controllers
+{
+    [Route("status")]
+    public class StatusController
+    {
+        [HttpGet("")]
+        public ActionResult GetCurrentUser()
+        {
+            var status = new
+            {
+                Status = "Ok",
+                DatabaseStatus = "Ok",
+                MySqlConnected = "False"
+            };
+            return new JsonResult(status);
+        }
+    }
+}
